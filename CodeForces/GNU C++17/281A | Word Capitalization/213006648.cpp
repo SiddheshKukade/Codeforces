2d4181c622dc5e29c01efe914bdc6bcbbc0a763a
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-07-10 00:46:24 +0530

    Committing files

---
+++ CodeForces/GNU C++17/281A | Word Capitalization/213006648.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ld long double
+#define ret(s) {cout<<s<<endl; return;}
+#define fr(i, a, n) for(ll i = a; i < n; i++)
+#define frr(i, a, n) for(ll i = a; i >= n; i--)
+ 
+void solve(){
+ 
+}
+ 
+int main(){
+    // ll t = 1;
+    // while(t--) solve();
+ string s;
+ cin>>s; 
+ if(islower(s[0])) s[0] = toupper(s[0]);
+ cout<<s<<endl;
+ 
+    return 0;
+}
