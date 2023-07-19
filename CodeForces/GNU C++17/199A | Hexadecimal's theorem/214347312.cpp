c8306db96c7aaae3488b51f39d877cc89a746e76
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-07-19 12:55:49 +0530

    Committing files

---
+++ CodeForces/GNU C++17/199A | Hexadecimal's theorem/214347312.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+// Author: SiddheshKukade
+int main() {
+ long long n; 
+ cin>>n;
+//  N IS A FIBONACCI
+ cout<<0<<" "<<0<<" "<<n; 
+ 
+ return 0; 
+}
+ 
