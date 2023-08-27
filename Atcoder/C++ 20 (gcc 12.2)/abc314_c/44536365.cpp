21ee8d1874c7194047cb9a2ff174d9de5ea0dd01
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-08-27 12:26:46 +0530

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/abc314_c/44536365.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n, m;
+string s;
+int c[200000];
+vector<int> p[200001];
+int main(void)
+{
+    cin >> n >> m;
+    cin >> s;
+    for(int i = 0; i < n; i++) cin >> c[i];
+    for(int i = 0; i < n; i++) p[c[i]].push_back(i);
+    
+    string t(n, '?');
+    for(int i = 1; i <= m; i++){
+        int k = p[i].size();
+        for(int j = 0; j < k; j++) t[p[i][(j+1)%k]] = s[p[i][j]];
+    }
+    cout << t << endl;
+    
+    return 0;
+}
